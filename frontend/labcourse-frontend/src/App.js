9b7e2361d55478f8deb72e2651c62b5d02ef2922
etnapireva <149309614+etnapireva@users.noreply.github.com>
2025-04-27 13:59:01 +0200

    Frontend1

---
+++ frontend/labcourse-frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Prove</h1>
+                <p>labcourse frontend</p>
+                <button onClick={() => alert('Click here')}>
+                    Click here
+                </button>
+            </header>
+        </div>
+    );
+}
+
+export default App;
